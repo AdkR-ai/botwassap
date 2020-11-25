70c2a55902acea0fb82826a4540e8698ea777530
AdkR-ai <74963365+AdkR-ai@users.noreply.github.com>
2020-11-25 16:57:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'NICO BOT🤖'; 
+const instagram = 'https://instagram.com/eggstar_craft'; 
+const telegram = 'https://t.me/gkpunyatelegram'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/Andikunz';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
